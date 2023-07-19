29360793a83d054ef8f39f2c6e197bc0629039b7
entellaKa <inchol321@gmail.com>
2023-07-19 16:36:18 +0900

    call multiplication_table() from main()

--- main.c
+++ main.c
@@ -1,8 +1,14 @@
 #include<stdio.h>
 
-void multiplication_table();
+void multiplication_table(){
+	for (int i = 1; i <= 9; i++){
+		for (int j = 2; j <= 9; j++){
+			printf("%d * %d = %d\t", j, i, i*j);
+		}
+	}
+}
 
 int main(){
-
+	multiplication_table()
 	return 0;
 }
