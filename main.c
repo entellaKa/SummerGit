a47a106ef0b1ac251463732e55075688c648f41e
entellaKa <inchol321@gmail.com>
2023-07-20 15:31:48 +0900

    commit

--- main.c
+++ main.c
@@ -1,6 +1,5 @@
 #include<stdio.h>
 
-<<<<<<< HEAD
 void multiplication_table(){
 	for (int i = 1; i <= 9; i++){
 		for (int j = 2; j <= 9; j++){
@@ -9,15 +8,12 @@ void multiplication_table(){
 	}
 }
 
-int main(){
-	multiplication_table()
-=======
 void func2(){
 	printf("Hello, World!!\n");
 }
 
 int main(){
+	multiplication_table();
 	func2();
->>>>>>> 815558c (wtf)
 	return 0;
 }
